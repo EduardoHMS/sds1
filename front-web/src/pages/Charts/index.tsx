b584ea494581c833f20e3ceacd3bd47a86ea2622
Eduardo Henrique <eduardohenriquemoreirasilva@gmail.com>
2020-09-21 15:15:27 -0300

    Configurando front-web para produção

--- front-web/src/pages/Charts/index.tsx
+++ front-web/src/pages/Charts/index.tsx
@@ -20,7 +20,7 @@ const initialPieData = {
     labels: [],
     series: []
 }
-const BASE_URL = 'http://localhost:8080'
+const BASE_URL = 'https://sds1-eduardo-henrique.herokuapp.com';
 
 const Charts = () => {
 
