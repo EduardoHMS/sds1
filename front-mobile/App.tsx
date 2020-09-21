fb17f4c95bd4aeff410040053b52e99a78fb34f5
Eduardo Henrique <eduardohenriquemoreirasilva@gmail.com>
2020-09-20 21:22:20 -0300

    Adicionando rotas no React native

--- front-mobile/App.tsx
+++ front-mobile/App.tsx
@@ -9,7 +9,7 @@ import {
 } from '@expo-google-fonts/play';
 
 import Header from './src/components/Header'
-import Home from './src/pages/Home';
+import Routes from './src/routes';
 
 
 export default function App() {
@@ -22,8 +22,7 @@ export default function App() {
   }else{
     return (
       <View style={styles.container}>
-        <Header />
-        <Home />
+        <Routes />
         <StatusBar style="light" />
       </View>
     );
@@ -33,6 +32,5 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#0B1F34'
   },
 });
---
+++ front-mobile/src/pages/CreateRecord/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { StyleSheet, Text } from 'react-native';
+import Header from '../../components/Header';
+
+const CreateRecord = () => {
+    return (
+        <>
+            <Header />
+        </>
+    
+    )
+};
+
+const styles = StyleSheet.create({
+    text: {
+      color: 'white',
+      fontSize: 50,
+      textAlign: 'center',
+      marginTop: 100
+    },
+});
+
+export default CreateRecord;
