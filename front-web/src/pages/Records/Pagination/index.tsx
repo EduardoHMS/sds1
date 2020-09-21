495137f74bec72edef8ee15e2887a8b702c73b7a
Eduardo Henrique <eduardohenriquemoreirasilva@gmail.com>
2020-09-21 15:46:19 -0300

    Corrigindo erro da pagina index.tsx

--- front-web/src/pages/Records/Pagination/index.tsx
+++ front-web/src/pages/Records/Pagination/index.tsx
@@ -12,7 +12,7 @@ const Pagination = ({ totalPages = 0, goToPage, activePage}: Props) => {
     return (
         <div className="pagination-container">
             {paginationItems.map(item => (
-                <button key={item} className={`pagination-item ${activePage == item ? 'active' : 'inactive'}`} onClick={() => goToPage(item)}>{item + 1}</button>
+                <button key={item} className={`pagination-item ${activePage === item ? 'active' : 'inactive'}`} onClick={() => goToPage(item)}>{item + 1}</button>
             ))}
             
         </div>
